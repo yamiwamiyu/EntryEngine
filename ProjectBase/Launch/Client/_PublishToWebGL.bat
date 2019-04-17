241d5101b6b467086c2c22776e1545457833852c
yamiwamiyu <359602182@qq.com>
2019-04-17 13:41:28 +0800

    发布WebGL批处理和相应目录结构

---
+++ ProjectBase/Launch/Client/_PublishToWebGL.bat
@@ -0,0 +1,2 @@
+..\..\EntryBuilder PublishToWebGL ..\..\..\..\ ..\..\Code\Client\ ..\..\Publish\WebGL\index.html true 0
+xcopy /D /Y /E Content\*.* ..\..\Publish\WebGL\Content\
---
+++ ProjectBase/Launch/Client/_RunWebGL(运行不了请用管理员运行).bat
@@ -0,0 +1,3 @@
+cd %~dp0
+..\..\Publish\WebGL\SimpleHttpService.exe 65535
+start http://localhost:65535/index.html
