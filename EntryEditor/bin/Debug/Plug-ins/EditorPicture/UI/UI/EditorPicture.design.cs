e5776133fb589d4d3dd3103ad8c74c5e47977e04
yamiwamiyu <359602182@qq.com>
2019-10-17 17:04:05 +0800

    大图编辑器完成

--- EntryEditor/bin/Debug/Plug-ins/EditorPicture/UI/UI/EditorPicture.design.cs
+++ EntryEditor/bin/Debug/Plug-ins/EditorPicture/UI/UI/EditorPicture.design.cs
@@ -9,6 +9,8 @@ public partial class EditorPicture
     public EntryEngine.UI.Label LScale = new EntryEngine.UI.Label();
     public EntryEngine.UI.Label LPosition = new EntryEngine.UI.Label();
     public EntryEngine.UI.Label LHelp = new EntryEngine.UI.Label();
+    public EntryEngine.UI.TextBox TBSizeWidth = new EntryEngine.UI.TextBox();
+    public EntryEngine.UI.TextBox TBSizeHeight = new EntryEngine.UI.TextBox();
     
     
     private void Initialize()
@@ -133,6 +135,7 @@ public partial class EditorPicture
         LHelp_Clip.Height = 0;
         LHelp.Clip = LHelp_Clip;
         
+        LHelp.Anchor = (EAnchor)6;
         LHelp.UIText = new EntryEngine.UI.UIText();
         LHelp.UIText.Text = "操作方法";
         LHelp.UIText.FontColor = new COLOR()
@@ -149,6 +152,98 @@ public partial class EditorPicture
         LHelp.UIText.FontSize = 14f;
         LHelp.Text = "操作方法";
         this.Add(LHelp);
+        TBSizeWidth.Name = "TBSizeWidth";
+        EntryEngine.RECT TBSizeWidth_Clip = new EntryEngine.RECT();
+        TBSizeWidth_Clip.X = 61f;
+        TBSizeWidth_Clip.Y = 20f;
+        TBSizeWidth_Clip.Width = 40f;
+        TBSizeWidth_Clip.Height = 20f;
+        TBSizeWidth.Clip = TBSizeWidth_Clip;
+        
+        TBSizeWidth.Color = new COLOR()
+        {
+            B = 0,
+            G = 0,
+            R = 0,
+            A = 0,
+        };
+        TBSizeWidth.DefaultText = new EntryEngine.UI.UIText();
+        TBSizeWidth.DefaultText.Text = "";
+        TBSizeWidth.DefaultText.FontColor = new COLOR()
+        {
+            B = 211,
+            G = 211,
+            R = 211,
+            A = 255,
+        };
+        TBSizeWidth.DefaultText.TextAlignment = (EPivot)0;
+        TBSizeWidth.DefaultText.TextShader = null;
+        TBSizeWidth.DefaultText.Padding.X = 0f;
+        TBSizeWidth.DefaultText.Padding.Y = 0f;
+        TBSizeWidth.DefaultText.FontSize = 16f;
+        TBSizeWidth.UIText = new EntryEngine.UI.UIText();
+        TBSizeWidth.UIText.Text = "999999";
+        TBSizeWidth.UIText.FontColor = new COLOR()
+        {
+            B = 255,
+            G = 255,
+            R = 255,
+            A = 255,
+        };
+        TBSizeWidth.UIText.TextAlignment = (EPivot)17;
+        TBSizeWidth.UIText.TextShader = null;
+        TBSizeWidth.UIText.Padding.X = 0f;
+        TBSizeWidth.UIText.Padding.Y = 0f;
+        TBSizeWidth.UIText.FontSize = 12f;
+        TBSizeWidth.Text = "999999";
+        TBSizeWidth.MaxLength = 6;
+        this.Add(TBSizeWidth);
+        TBSizeHeight.Name = "TBSizeHeight";
+        EntryEngine.RECT TBSizeHeight_Clip = new EntryEngine.RECT();
+        TBSizeHeight_Clip.X = 106f;
+        TBSizeHeight_Clip.Y = 20f;
+        TBSizeHeight_Clip.Width = 40f;
+        TBSizeHeight_Clip.Height = 20f;
+        TBSizeHeight.Clip = TBSizeHeight_Clip;
+        
+        TBSizeHeight.Color = new COLOR()
+        {
+            B = 0,
+            G = 0,
+            R = 0,
+            A = 0,
+        };
+        TBSizeHeight.DefaultText = new EntryEngine.UI.UIText();
+        TBSizeHeight.DefaultText.Text = "";
+        TBSizeHeight.DefaultText.FontColor = new COLOR()
+        {
+            B = 211,
+            G = 211,
+            R = 211,
+            A = 255,
+        };
+        TBSizeHeight.DefaultText.TextAlignment = (EPivot)0;
+        TBSizeHeight.DefaultText.TextShader = null;
+        TBSizeHeight.DefaultText.Padding.X = 0f;
+        TBSizeHeight.DefaultText.Padding.Y = 0f;
+        TBSizeHeight.DefaultText.FontSize = 16f;
+        TBSizeHeight.UIText = new EntryEngine.UI.UIText();
+        TBSizeHeight.UIText.Text = "999999";
+        TBSizeHeight.UIText.FontColor = new COLOR()
+        {
+            B = 255,
+            G = 255,
+            R = 255,
+            A = 255,
+        };
+        TBSizeHeight.UIText.TextAlignment = (EPivot)17;
+        TBSizeHeight.UIText.TextShader = null;
+        TBSizeHeight.UIText.Padding.X = 0f;
+        TBSizeHeight.UIText.Padding.Y = 0f;
+        TBSizeHeight.UIText.FontSize = 12f;
+        TBSizeHeight.Text = "999999";
+        TBSizeHeight.MaxLength = 6;
+        this.Add(TBSizeHeight);
         
         this.PhaseShowing += Show;
     }
@@ -160,6 +255,12 @@ public partial class EditorPicture
         
         
         
+        
+        
+        TBSizeWidth.SourceNormal = PATCH.GetNinePatch(PATCH.NullColor, new COLOR(255, 255, 255, 255), 1);
+        
+        
+        TBSizeHeight.SourceNormal = PATCH.GetNinePatch(PATCH.NullColor, new COLOR(255, 255, 255, 255), 1);
         
         
         
