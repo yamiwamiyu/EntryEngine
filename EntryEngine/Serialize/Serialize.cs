0d68d5914576692597e4e5b6bd7a13ffd9db7a75
yamiwamiyu <359602182@qq.com>
2020-04-22 15:55:38 +0800

    修复Json解析string[]成int[]时死循环的BUG

--- EntryEngine/Serialize/Serialize.cs
+++ EntryEngine/Serialize/Serialize.cs
@@ -1216,26 +1216,25 @@ namespace EntryEngine.Serialize
         }
         protected virtual object ReadNumber(Type type)
         {
-            return _SERIALIZE.ParseNumber(type, ReadNextString());
-            //char c = PeekChar;
-            //switch (c)
-            //{
-            //    case '0':
-            //    case '1':
-            //    case '2':
-            //    case '3':
-            //    case '4':
-            //    case '5':
-            //    case '6':
-            //    case '7':
-            //    case '8':
-            //    case '9':
-            //    case '-':
-            //    case '.':
-            //        return _SERIALIZE.ParseNumber(type, ReadNextString());
+            char c = PeekChar;
+            switch (c)
+            {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '-':
+                case '.':
+                    return _SERIALIZE.ParseNumber(type, ReadNextString());
 
-            //    default: throw new FormatException("错误的数字格式");
-            //}
+                default: throw new FormatException("错误的数字格式");
+            }
         }
         protected virtual TimeSpan ReadTimeSpan()
         {
