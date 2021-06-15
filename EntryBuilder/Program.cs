4227fbbe367989cde2998cf02752b4c572bd15c3
yamiwamiyu <359602182@qq.com>
2021-06-15 14:49:00 +0800

    生成前端接口修改

--- EntryBuilder/Program.cs
+++ EntryBuilder/Program.cs
@@ -2069,99 +2069,110 @@ namespace EntryBuilder
                 builder.AppendBlock(() =>
                 {
                     builder.AppendLine(
-@"  /// <summary>请求的公共地址，例如 http://127.0.0.1:888/Action/ </summary>
-    public string base_url = """";
-    /// <summary>在发送请求之前执行</summary>
-    public Action<HttpRequestPost> onSend = null;
-    /// <summary>在回调之前执行</summary>
-    public Action<HttpRequestPost> onCallback = null;
-    /// <summary>处理接口返回错误</summary>
-    public Action<string> onErrorMsg = null;
-    /// <summary>处理请求错误</summary>
-    public Action<HttpRequestPost> onError = null;
-    /// <summary>回调函数异步还是同步执行，true为异步</summary>
-    public bool async = true;");
+@"/// <summary>请求的公共地址，例如 http://127.0.0.1:888/Action/ </summary>
+public string Host;
+/// <summary>在发送请求之前执行</summary>
+public Action<HttpRequestPost> OnSend;
+/// <summary>在回调之前执行</summary>
+public Action<string> OnCallback;
+/// <summary>接口返回HttpError实例或口返回状态码不为200(OnError有指定则仅执行OnError)时</summary>
+public Action<int, string> OnHttpError;
+/// <summary>接口返回状态码不为200或请求抛出WebException时</summary>
+public Action<HttpRequestPost, WebException> OnError;
+/// <summary>回调函数异步还是同步执行，true为异步</summary>
+public bool IsAsync = true;
+
+private void _onError(HttpRequestPost req, WebException e, byte[] resultBytes)
+{
+    if (OnError != null) OnError(req, e);
+    else _LOG.Error(e, ""发送请求错误 {0}"", req.Request.RequestUri.ToString());
+}");
                     builder.AppendLine();
 
                     builder.AppendLine(
-@"  /// <summary>发送HTTP请求</summary>
-    /// <param name=""url"">请求的地址，例如 1/GetInfo</param>
-    /// <param name=""data"">请求的参数</param>
-    /// <param name=""callback"">请求完成后的回调函数</param>
-    private AsyncData<string> send<T>(string url, string data, Action<T> callback)");
+@"/// <summary>发送HTTP请求</summary>
+/// <param name=""url"">请求的地址，例如 1/GetInfo</param>
+/// <param name=""data"">请求的参数</param>
+/// <param name=""callback"">请求完成后的回调函数</param>
+private AsyncData<string> send<T>(string url, string data, Action<T> callback)");
 
                     builder.AppendBlock(() =>
                     {
                         builder.AppendLine(
-@"      AsyncData<string> result = new AsyncData<string>();
+@"AsyncData<string> result = new AsyncData<string>();
 
-        var request = new HttpRequestPost();
-        var requestURL = base_url + url;
-        request.Connect(requestURL);
+var request = new HttpRequestPost();
+var requestURL = Host + url;
+request.Connect(requestURL);
 
-        if (onSend != null)
-            onSend(request);
+if (OnSend != null)
+{
+    OnSend(request);
+}
 
-        request.Send(Encoding.UTF8.GetBytes(data));
+request.OnError += _onError;
+request.OnReceived += (response) =>
+{
+    if (response.StatusCode == HttpStatusCode.OK)
+    {
+        string ret = _IO.ReadPreambleText(response.GetResponseStream(), Encoding.UTF8);
 
-        request.OnReceived += (response) =>
+        if (ret.StartsWith(""{\""errCode\""""))
         {
-            if (response.StatusCode == HttpStatusCode.OK)
+            HttpError err = JsonReader.Deserialize<HttpError>(ret);
+            if (OnHttpError != null)
             {
-                string tempData = _IO.ReadPreambleText(_IO.ReadStream(response.GetResponseStream()), Encoding.UTF8);
-
-                tempData = !string.IsNullOrEmpty(tempData) ? tempData : ""{}"";
-                if (tempData.StartsWith(""{\""errCode\""""))
-                {
-                    if (onErrorMsg != null)
-                    {
-                        onErrorMsg(tempData);
-                    }
-                    else
-                    {
-                        _LOG.Warning(""未设置onErrorMsg: {0}"", tempData);
-                    }
-                }
-                else
-                {
-                    if (onCallback != null)
-                    {
-                        onCallback(request);
-                    }
-                    if (callback != null)
-                    {
-                        if (async)
-                        {
-                            callback(JsonReader.Deserialize<T>(tempData));
-                        }
-                        else
-                        {
-                            EntryService.Instance.Synchronize(() => callback(JsonReader.Deserialize<T>(tempData)));
-                        }
-                    }
-                    else
-                    {
-                        _LOG.Warning(""未设置回调函数: {0}"",tempData);
-                    }
-                }
-                result.SetData(tempData);
-            }
-            else if (onError != null)
-            {
-                 onError(request);
+                OnHttpError(err.errCode, err.errMsg);
             }
             else
             {
-                _LOG.Warning(""State: {0} {1}"", response.StatusCode, response.StatusDescription);
+                _LOG.Warning(""未设置错误回调函数: {0} {1}"", err.errCode, err.errMsg);
             }
-        };
-
-        request.OnError += (req, e, resultBytes) =>
+        }
+        else
         {
-            _LOG.Error(e, ""发送请求错误 {0}"", requestURL);
-        };
+            if (OnCallback != null)
+            {
+                OnCallback(ret);
+            }
+            if (callback != null)
+            {
+                T retObj = JsonReader.Deserialize<T>(ret);
+                if (IsAsync)
+                {
+                    callback(retObj);
+                }
+                else
+                {
+                    EntryService.Instance.Synchronize(() => callback(retObj));
+                }
+            }
+            else
+            {
+                _LOG.Warning(""未设置回调函数: {0}"", ret);
+            }
+        }
+        result.SetData(ret);
+    }
+    else if (OnError != null)
+    {
+         OnError(request, null);
+    }
+    else
+    {
+        if (OnHttpError != null)
+        {
+            OnHttpError((int)response.StatusCode, response.StatusDescription);
+        }
+        else
+        {
+            _LOG.Warning(""未设置错误回调函数: {0} {1}"", response.StatusCode, response.StatusDescription);
+        }
+    }
+};
+request.Send(Encoding.UTF8.GetBytes(data));
 
-        return result;"
+return result;"
                        );
                     });
 
