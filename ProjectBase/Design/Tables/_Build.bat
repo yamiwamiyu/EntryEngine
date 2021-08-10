9c051f5648e5576733b428ed2601dbc1d4bc6163
yamiwamiyu <359602182@qq.com>
2021-08-10 10:14:49 +0800

    最新开源方式发布项目

--- ProjectBase/Design/Tables/_Build.bat
+++ ProjectBase/Design/Tables/_Build.bat
@@ -4,6 +4,6 @@ del /q ..\Tables_Build\*.xlsx
 :: 构建数据表
 :: 构建数据表到客户端
 copy *.xlsx ..\Tables_Build\
-..\..\EntryBuilder.exe BuildCSVFromExcel ..\Tables_Build\ ..\..\Launch\Client\Content\Tables\ ..\LANGUAGE.csv 12.0 ..\..\Code\Protocol\Protocol\_TABLE.design.cs true
+..\..\EntryBuilder.exe BuildCSVFromExcel ..\Tables_Build\ ..\..\Launch\Client\Content\Tables\ ..\LANGUAGE.csv 12.0 ..\..\Code\Protocol\_TABLE.design.cs true
 :: 复制数据表到服务端
 copy ..\..\Launch\Client\Content\Tables\*.csv ..\..\Launch\Server\Tables\
--- ProjectBase/Design/Tables/_BuildConstant.bat
+++ ProjectBase/Design/Tables/_BuildConstant.bat
@@ -6,7 +6,7 @@ del /q ..\..\Launch\Server\Tables\CS.xml
 copy _C.xlsx ..\Tables_Build\
 copy _CC.xlsx ..\Tables_Build\
 copy _CS.xlsx ..\Tables_Build\
-..\..\EntryBuilder.exe BuildConstantTable ..\Tables_Build\_C.xlsx ..\..\Launch\Server\Tables\C.xml ..\..\Code\Protocol\Protocol\_C.design.cs true 12.0
+..\..\EntryBuilder.exe BuildConstantTable ..\Tables_Build\_C.xlsx ..\..\Launch\Server\Tables\C.xml ..\..\Code\Protocol\_C.design.cs true 12.0
 ..\..\EntryBuilder.exe BuildConstantTable ..\Tables_Build\_CC.xlsx ..\..\Launch\Client\Content\Tables\CC.xml ..\..\Code\Client\Client\_CC.design.cs true 12.0
-..\..\EntryBuilder.exe BuildConstantTable ..\Tables_Build\_CS.xlsx ..\..\Launch\Server\Tables\CS.xml ..\..\Code\Server\Server\_CS.design.cs true 12.0
+..\..\EntryBuilder.exe BuildConstantTable ..\Tables_Build\_CS.xlsx ..\..\Launch\Server\Tables\CS.xml ..\..\Code\Server\_CS.design.cs true 12.0
 copy ..\..\Launch\Server\Tables\C.xml ..\..\Launch\Client\Content\Tables\
