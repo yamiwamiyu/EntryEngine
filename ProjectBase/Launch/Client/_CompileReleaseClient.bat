56aa72b4db0f35a423399a7d000db88f201c022d
yamiwamiyu <359602182@qq.com>
2021-08-27 10:47:27 +0800

    资源加密发布

--- ProjectBase/Launch/Client/_CompileReleaseClient.bat
+++ ProjectBase/Launch/Client/_CompileReleaseClient.bat
@@ -1,4 +1,4 @@
-:: 使用Release编译项目，可以把使用大图，服务器IP的代码放入#if !Debug块中
+:: 使用Release编译项目，可以把使用大图，正式服IP，资源解密的代码放入#if !Debug块中
 cd ..\..\
 EntryBuilder BuildDll Code\Client\Client\ Launch\Client\Client.dll 3.5 "Launch\Client\Protocol.dll|Launch\Client\DragonBone.dll" false Release
 cd Launch\Client\
--- ProjectBase/Publish/_版本发布-Unity.bat
+++ ProjectBase/Publish/_版本发布-Unity.bat
@@ -3,9 +3,26 @@ if not exist __Unity (
 )
 svn update ..\Launch\Client
 cd ..\Launch\Client\
+:: Release编译项目
+call _CompileReleaseClient.bat
 call _PublishToUnity.bat
 cd /d %~dp0
-xcopy /S /Y ..\Publish\Project\Assets\StreamingAssets\*.* __Unity\
-del /S /Q __Unity\*.meta
+
+:: 资源加密发布
+md __UnityTemp
+xcopy /S /Y ..\Publish\Project\Assets\StreamingAssets\*.* __UnityTemp\
+del /S /Q __UnityTemp\*.meta
+move __UnityTemp\__filelist.txt __Unity\
+move __UnityTemp\__version.txt __Unity\
+move __UnityTemp\web.config __Unity\
+xcopy /Y __UnityTemp\*.bytes __Unity\
+del /Q __UnityTemp\*.bytes __Unity\
+..\..\EntryBuilder BuildEncrypt __UnityTemp __Unity
+rd /S /Q __UnityTemp
+
+:: 直接发布
+::xcopy /S /Y ..\Publish\Project\Assets\StreamingAssets\*.* __Unity\
+::del /S /Q __UnityTemp\*.meta
+
 ::svn add __Unity --auto-props --force
 ::svn commit __Unity -m "Publish __Unity Commit"
