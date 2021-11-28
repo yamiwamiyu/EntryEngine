bc3b65c5b8bcb884baf4e22a71c9895f9740aa49
yamiwamiyu <359602182@qq.com>
2021-11-28 13:15:02 +0800

    修复多层Mask弹窗没有使用不同实例的BUG

--- EntryEngine/UI/AnimationScene.cs
+++ EntryEngine/UI/AnimationScene.cs
@@ -198,7 +198,14 @@ namespace EntryEngine.UI
 
         public static T Show<T>() where T : UIScene, new()
         {
-            SMask gray = Entry.Instance.ShowDialogScene<SMask>(EState.Dialog);
+            SMask gray;
+            if (Entry.Instance.GetSceneOrCreate<SMask>().IsInStage)
+            {
+                gray = new SMask();
+                Entry.Instance.ShowDialogScene(gray, EState.Dialog);
+            }
+            else
+                gray = Entry.Instance.ShowDialogScene<SMask>(EState.Dialog);
             T scene = Entry.Instance.ShowDialogScene<T>(EState.None);
             gray.Scene = scene;
             return scene;
