64df9b0ce9aebc73dfc442dcf3d34ab8b4a3993e
yamiwamiyu <359602182@qq.com>
2022-03-29 14:41:29 +0800

    开源项目启动命令

--- ProjectBase/Launch/Server/_AutoLaunch.bat
+++ ProjectBase/Launch/Server/_AutoLaunch.bat
@@ -1,3 +1,3 @@
 cd /d %~dp0
 :: 需要改数据库的名字，则修改dbname即可
-Cmdline.exe "Launch 888 Server=127.0.0.1;Port=3306;User=root;Password=123456; dbname" "LoadTable"
+Server.exe "SetDatabase Server=127.0.0.1;Port=3306;User=root;Password=123456; dbname true" "Launch 888"
