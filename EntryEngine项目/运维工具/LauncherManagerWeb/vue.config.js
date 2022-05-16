6de8b219c91baf553cf26b8d5aad8b6f8ba430c5
yamiwamiyu <10332709+yamiwamiyu@user.noreply.gitee.com>
2022-05-16 16:18:12 +0800

    1. 运维工具前端发布时不生成.map文件 2. 发布端在dist文件夹中

--- EntryEngine项目/运维工具/LauncherManagerWeb/vue.config.js
+++ EntryEngine项目/运维工具/LauncherManagerWeb/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  productionSourceMap: false,
 
   css: {
     loaderOptions: {
