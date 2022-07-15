24549001f317b283ac6582121873bac4d57764a8
yamiwamiyu <10332709+yamiwamiyu@user.noreply.gitee.com>
2022-07-15 11:27:37 +0800

    新增Foreach方法，将var改为let

--- EntryEngine项目/DotNet.js
+++ EntryEngine项目/DotNet.js
@@ -1,9 +1,10 @@
+console.log('引入global.js')
 // 查找
 
 // 查找符合条件的第一个元素，没有符合的返回undefined
 // Boolean func(value)，func可不传，此时返回第一个元素
 Object.prototype._First = function(func) {
-    for (var value of this)
+    for (let value of this)
         if (!func || func(value))
             return value;
 }
@@ -16,8 +17,8 @@ Object.prototype._Last = function(func) {
         else
             return this[this.length - 1];
     } else {
-        var last;
-        for (var value of this)
+        let last;
+        for (let value of this)
             if (!func || func(value))
                 last = value;
         return last;
@@ -26,14 +27,14 @@ Object.prototype._Last = function(func) {
 // 查找符合条件的所有元素，没有符合的返回undefined
 // Boolean func(value)
 Object.prototype._Where = function*(func) { 
-    for (var value of this) 
+    for (let value of this) 
         if (func(value))
             yield value;
 }
 // 数组所有元素均为true时则为true，空数组为true
 // Boolean func(value)
 Object.prototype._All = function(func) { 
-	for (var value of this)
+	for (let value of this)
 		if (!func(value))
 			return false;
 	return true;
@@ -41,7 +42,7 @@ Object.prototype._All = function(func) {
 // 数组任意元素为true时则为true，空数组为false
 // Boolean func(value)
 Object.prototype._Any = function(func) { 
-	for (var value of this)
+	for (let value of this)
 		if (func(value))
 			return true;
 	return false;
@@ -53,16 +54,16 @@ Object.prototype._Any = function(func) {
 // 选择迭代器元素中的某个字段作为新的迭代器
 // Any func(value)
 Object.prototype._Select = function*(func) { 
-    for (var value of this) 
+    for (let value of this) 
         yield func(value);
 }
 // 联合迭代器元素中的某个迭代器字段作为新的迭代器
 // Any func(value)
 Object.prototype._SelectMany = function*(func) {
-    for (var value of this) {
-        var many = func(value);
+    for (let value of this) {
+        let many = func(value);
         if (many)
-            for (var value2 of many)
+            for (let value2 of many)
                 yield value2;
     }
 }
@@ -70,9 +71,9 @@ Object.prototype._SelectMany = function*(func) {
 // Any funcKey(value)
 // Any funcValue(value)，funcValue可不传，值就是原始迭代器的元素
 Object.prototype._GroupBy = function(funcKey, funcValue) {
-    var group = [];
-    var key;
-    for (var value of this) {
+    let group = [];
+    let key;
+    for (let value of this) {
         key = funcKey(value);
         if (!group[key])
             group[key] = [];
@@ -85,10 +86,17 @@ Object.prototype._GroupBy = function(funcKey, funcValue) {
 }
 // 将迭代器转换成数组，若原本是数组，也会返回一个新数组
 Object.prototype._ToArray = function() {
-    var array = [];
-    for (var value of this)
+    let array = [];
+    for (let value of this)
         array.push(value);
     return array;
+}
+// 将迭代器迭代一遍，返回原本的迭代器
+// void func(value)
+Object.prototype._Foreach = function(func) {
+    for (let value of this)
+        func(value)
+    return this;
 }
 
 // 聚合函数
@@ -98,8 +106,8 @@ Object.prototype._ToArray = function() {
 Object.prototype._Count = function(func) {
     if (!func && this instanceof Array)
         return this.length;
-    var count = 0;
-    for (var value of this)
+    let count = 0;
+    for (let value of this)
         if (!func || func(value))
             count++;
     return count;
@@ -107,15 +115,15 @@ Object.prototype._Count = function(func) {
 // 计算数组所有元素的平均值
 // Number func(value)，func可不传，此时this必须是Number类型的迭代器
 Object.prototype._Avg = function(func) {
-    var sum = 0;
-    var count = 0;
+    let sum = 0;
+    let count = 0;
     if (func) {
-	    for (var value of this) {
+        for (let value of this) {
             sum += func(value);
             count++;
         }
     } else {
-        for (var value of this) {
+        for (let value of this) {
             sum += value;
             count++;
         }
@@ -128,12 +136,12 @@ Object.prototype._Avg = function(func) {
 // 计算数组所有元素的累加值
 // Number func(value)，func可不传，此时this必须是Number类型的迭代器
 Object.prototype._Sum = function(func) {
-    var sum = 0;
+    let sum = 0;
     if (func)
-	    for (var value of this)
+        for (let value of this)
             sum += func(value);
     else
-        for (var value of this)
+        for (let value of this)
             sum += value;
     return sum;
 }
@@ -142,12 +150,12 @@ Object.prototype._Sum = function(func) {
 // e，默认为true，为true时返回元素，否则返回数值
 Object.prototype._Max = function(func, e = true) {
     // 最大值对应的对象
-    var result;
+    let result;
     if (func) {
         // 最大值
-        var rvalue;
-        var temp;
-        for (var value of this) {
+        let rvalue;
+        let temp;
+        for (let value of this) {
             temp = func(value);
             if (rvalue == undefined || temp > rvalue) {
                 rvalue = temp;
@@ -157,7 +165,7 @@ Object.prototype._Max = function(func, e = true) {
         if (!e)
             result = rvalue;
     } else {
-        for (var value of this) {
+        for (let value of this) {
             if (result == undefined || value > result)
                 result = value;
         }
@@ -169,12 +177,12 @@ Object.prototype._Max = function(func, e = true) {
 // e，默认为true，为true时返回元素，否则返回数值
 Object.prototype._Min = function(func, e = true) {
     // 最小值对应的对象
-    var result;
+    let result;
     if (func) {
         // 最大值
-        var rvalue;
-        var temp;
-        for (var value of this) {
+        let rvalue;
+        let temp;
+        for (let value of this) {
             temp = func(value);
             if (rvalue == undefined || temp < rvalue) {
                 rvalue = temp;
@@ -184,7 +192,7 @@ Object.prototype._Min = function(func, e = true) {
         if (!e)
             result = rvalue;
     } else {
-        for (var value of this) {
+        for (let value of this) {
             if (result == undefined || value < result)
                 result = value;
         }
@@ -197,9 +205,9 @@ Object.prototype._Min = function(func, e = true) {
 
 // 合并另一个迭代器
 Object.prototype._Concat = function*(iterator) {
-    for (var value of this)
+    for (let value of this)
         yield value;
-    for (var value of iterator)
+    for (let value of iterator)
         yield value;
 }
 // 跳过序列中指定数量的元素，然后返回剩余的元素
@@ -207,10 +215,10 @@ Object.prototype._Skip = function*(count) {
     if (count <= 0)
         return this;
     if (this instanceof Array) {
-        for (var i = count; i < this.length; i++)
+        for (let i = count; i < this.length; i++)
             yield this[i];
     } else {
-        for (var value of this) {
+        for (let value of this) {
             count--;
             if (count < 0)
                 yield value;
@@ -219,7 +227,7 @@ Object.prototype._Skip = function*(count) {
 }
 // 从序列的开头返回指定数量的连续元素
 Object.prototype._Take = function*(count) {
-    for (var value of this) {
+    for (let value of this) {
         count--;
         if (count >= 0)
             yield value;
@@ -230,8 +238,8 @@ Object.prototype._Take = function*(count) {
 // 只要满足指定的条件，就跳过序列中的元素，然后返回剩余元素
 // Boolean func(value)
 Object.prototype._SkipWhile = function*(func) {
-    var skip = true;
-    for (var value of this) {
+    let skip = true;
+    for (let value of this) {
         if (skip) {
             skip = func(value);
             if (skip)
@@ -243,7 +251,7 @@ Object.prototype._SkipWhile = function*(func) {
 // 只要满足指定的条件，就会返回序列的元素
 // Boolean func(value)
 Object.prototype._TakeWhile = function*(func) {
-    for (var value of this) {
+    for (let value of this) {
         if (func(value))
             yield value;
         else
