2dc705b02025a1982c332cda165bb84e46324e52
yamiwamiyu <10332709+yamiwamiyu@user.noreply.gitee.com>
2022-07-15 11:29:35 +0800

    新增Test项目

--- Test/Program.cs
+++ Test/Program.cs
@@ -20,109 +20,33 @@ namespace Test
 {
     class TestScene : UIScene
     {
-        Sprite st1 = new Sprite();
-        SpriteText st2 = new SpriteText();
+        Sprite sprite = new Sprite();
+
         SHADER shader;
         protected override IEnumerable<ICoroutine> Loading()
         {
-            Label label = new Label();
-            label.UIText.FontColor = COLOR.Black;
-            label.X = 200;
-            label.Y = 200;
-            label.Pivot = EPivot.MiddleCenter;
-            label.Text = "测试";
-            //label.Effect.DoMoveX(400, 0, 1).DoFadeIn(1).DoScale(5, 1, 1).DoRotate(-360, 0, 1);
-            label.Effect.DoScale(5, 1, 1);
-            Add(label);
+            sprite.Texture = Content.Load<TEXTURE>("Test.ps");
+            sprite.Position.X = 500;
+            sprite.Position.Y = 300;
 
-            TextureBox tbox = new TextureBox();
-            tbox.X = 400;
-            tbox.Y = 400;
-            tbox.Pivot = EPivot.MiddleCenter;
-            tbox.Texture = Content.Load<TEXTURE>("数值_0.png");
-            tbox.Effect.DoScale(10, 1, 1);
-            label.Add(tbox);
-
-            //Content.Load<DRAGONBONES>("dragonbones/战士.json");
-
-            this.Background = TEXTURE.Pixel;
-            //this.Color = COLOR.Black;
-            st1.Texture = Content.Load<TEXTURE>("数值_0.png");
-            st1.Position.X = 200;
-            st1.Position.Y = 200;
-            //st2.Font = Content.Load<FONT>("等宽数值.tfont");
-            st2.Font = new FontRich(st2.Font);
-            st2.Text = "012<34<Color=0;255;0;255; >5\n67</><Font=等宽数值.tfont;>8</>666";
-            st2.Area.X = 200;
-            st2.Area.Y = 400;
-            st2.Color = COLOR.CornflowerBlue;
-            st2.Font.FontSize = 72;
-            st2.Alignment = EPivot.TopLeft;
-            //shader = Content.Load<SHADER>("描边.shader");
+            shader = Content.Load<SHADER>("Test.shader");
             //ShaderStroke.Shader = Content.Load<SHADER>("描边.shader");
-            shader = new ShaderGray()
-                {
-                };
-
-            //UIScene scene = new UIScene();
-            //scene.Background = TEXTURE.Pixel;
-            //scene.Color = COLOR.Lime;
-            //scene.Width = 100;
-            //scene.Height = 100;
-            //scene.DragMode = EDragMode.Move;
-            //scene.DragInertia = 1;
-            //scene.Rebound = 1f;
-            //Entry.ShowDialogScene(scene, EState.Dialog);
-
-            db = Content.Load<DRAGONBONES>("野人.json");
-            db.Proxy.AddDBEventListener(EntryEngine.DragonBone.DBCore.EEventType.FRAME_EVENT,
-                e =>
-                {
-                    _LOG.Debug("Type: {0}, Name: {1} Frame:{2}", e.type, e.name, GameTime.Time.FrameID);
-                });
+            
             return base.Loading();
         }
-        DRAGONBONES db;
-        int a = 0;
         protected override void InternalEvent(Entry e)
         {
             base.InternalEvent(e);
-            if (e.INPUT.Pointer.IsRelease(0))
-            {
-                //STextHint.ShowHint("测试fyiwoupbujagiuew[iruwe");
-                db.Proxy.Animation.Play(db.Proxy.Animation.animationNames[a], -1);
-                a++;
-                if (a == db.Proxy.Animation.animationNames.Count)
-                    a = 0;
-            }
         }
         protected override void InternalDraw(GRAPHICS spriteBatch, Entry e)
         {
-            
             base.InternalDraw(spriteBatch, e);
             //shader.SetValue("View", (MATRIX)spriteBatch.GraphicsToCartesianMatrix());
-            spriteBatch.Begin(new ShaderAlpha() { Alpha = 0.9f });
-            st1.Draw();
+            spriteBatch.Begin(shader);
 
-            var position = __INPUT.PointerPosition;
-            bool contains = st2.Contains(position.X, position.Y);
-            if (contains) st2.Color = COLOR.Lime;
-            if (__INPUT.PointerIsClick(0))
-            {
-            }
-            st2.Draw();
-            if (contains) st2.Color = COLOR.CornflowerBlue;
+            sprite.Draw();
+
             spriteBatch.End();
-
-            spriteBatch.Draw(db, new VECTOR2(400, 800));
-
-            //db.Update(e.GameTime.ElapsedSecond);
-
-            //var result = (DRAGONBONES)((EntryEngine.DragonBone.DBCore.Armature)db.Proxy.Armature.GetSlots().FirstOrDefault(s => s.name == "野人大招").displayList[0]).display;
-            ////result.Update(e.GameTime.ElapsedSecond);
-            ////if (result.Proxy.Animation.lastAnimationState == null)
-            ////    result.Proxy.Animation.Play();
-            //spriteBatch.Draw(result, new VECTOR2(400, 800));
         }
     }
 
