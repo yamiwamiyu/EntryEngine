ee1b10b700e5390b06c7cc57cba216d80a26f1b9
yamiwamiyu <10332709+yamiwamiyu@user.noreply.gitee.com>
2022-08-07 17:25:53 +0800

    修改UI后重新编译代码

--- EntryEngine项目模板/UIEditor/_RebuildUI.bat
+++ EntryEngine项目模板/UIEditor/_RebuildUI.bat
@@ -1,11 +1,12 @@
 :: 拷贝代码文件到项目
 call _CopyToCode.bat
 :: 重新编译项目
-..\EntryBuilder.exe BuildDll ..\Code\Client\Client\ ..\Launch\Client\Client.dll 3.5 "..\Code\EntryEngine.dll" false ""
+cd ..\
+EntryBuilder.exe BuildDll Code\Client\Client\ Launch\Client\Client.dll 3.5 "" false ""
 :: 拷贝可能新生成的翻译表到运行目录
-copy ..\Design\LANGUAGE.csv ..\Launch\Client\Content\Tables\LANGUAGE.csv
+copy Design\LANGUAGE.csv Launch\Client\Content\Tables\LANGUAGE.csv
 :: 输出最终翻译表
-..\EntryBuilder.exe BuildOutputCSV ..\Launch\Client\Content\Tables\LANGUAGE.csv ""
+EntryBuilder.exe BuildOutputCSV Launch\Client\Content\Tables\LANGUAGE.csv ""
 :: 切换到运行目录运行应用客户端程序
-cd ..\Launch\Client
-call Xna.exe
+cd Launch\Client
+call PCRun.exe
